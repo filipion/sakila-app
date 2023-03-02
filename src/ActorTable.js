a39c2af01f0cab6b8f5047a3c2e9d1b5578d3e25
filipion <filip.alexandru.ion@gmail.com>
2023-03-02 12:52:04 +0100

    Efficient actor rerender

--- src/ActorTable.js
+++ src/ActorTable.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActorCard from './ActorCard';
 
 const ActorTable = (props) => {
   return (
-    <div actors={props.actors}>
+    <div>
       <table>
         <thead>
           <tr>
@@ -16,18 +16,35 @@ const ActorTable = (props) => {
         </thead>
         <tbody>
           {props.actors.map(actor => (
-            <tr key={actor.ActorId}>
-              <td>{actor.ActorId}</td>
-              <td>{actor.FirstName}</td>
-              <td>{actor.LastName}</td>
-              <td>{actor.LastUpdate}</td>
-              <ActorCard id={actor.ActorId} FirstName={actor.FirstName} LastName={actor.LastName} refresh={props.refresh}></ActorCard>
-            </tr>
+            <ActorRow key={actor.ActorId} actor={actor}></ActorRow>
           ))}
         </tbody>
       </table>
     </div>
   );
 };
+
+const ActorRow = (props) => {
+  const [actor, setActor] = useState(props.actor) 
+
+  async function refreshActor() {
+    console.log("Fetching 1 actor...")
+    const response = await fetch(`/actors/${actor.ActorId}`);
+    const data = await response.json();
+    setActor(data);
+  }
+
+  return (
+    <tr key={props.actor.ActorId}>
+      <td>{actor.ActorId}</td>
+      <td>{actor.FirstName}</td>
+      <td>{actor.LastName}</td>
+      <td>{actor.LastUpdate}</td>
+      <td>
+        <ActorCard id={actor.ActorId} FirstName={actor.FirstName} LastName={actor.LastName} refresh={refreshActor}/>
+      </td>
+    </tr>
+  )
+}
 
 export default ActorTable;
